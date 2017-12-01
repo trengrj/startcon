d09666da4a796b8c2a59a77fcd686e7005eaa67a
John Trengrove <john@trengrove.nz>
2017-12-02 10:59:09 +1100

    price

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
+// Place JavaScript code here...
+function changePrice(checkbox) {
+  if(checkbox.checked == true){
+      document.getElementById("milestone-price").innerHTML= '$14,370';
+  }else{
+      document.getElementById("milestone-price").innerHTML= '$12,240';
+ }
+}
+
 $(document).ready(function() {
 
-  // Place JavaScript code here...
+
 
 });
